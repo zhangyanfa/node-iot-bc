a089a4019aad034984173b165fbffbe0eeef1349
张艳发 <yanfaz@yanfa-mbp.local>
2017-07-31 13:57:04 +0800

    add demo video

--- app.js
+++ app.js
@@ -243,6 +243,40 @@ app.get("/api/assets",function(req, res) {
     res.send(paylaod);
 });
 
+var fs = require("fs");
+app.get('/video', function(req, res) {
+  //const path = 'assets/sample.mp4'
+  var path = '/Users/yanfaz/Downloads/demo.mp4';
+  //var path = "/mnt/iot-bc/demo.mp4";
+  const stat = fs.statSync(path)
+  const fileSize = stat.size
+  const range = req.headers.range
+  if (range) {
+    const parts = range.replace(/bytes=/, "").split("-")
+    const start = parseInt(parts[0], 10)
+    const end = parts[1] 
+      ? parseInt(parts[1], 10)
+      : fileSize-1
+    const chunksize = (end-start)+1
+    const file = fs.createReadStream(path, {start, end})
+    const head = {
+      'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+      'Accept-Ranges': 'bytes',
+      'Content-Length': chunksize,
+      'Content-Type': 'video/mp4',
+    }
+    res.writeHead(206, head);
+    file.pipe(res);
+  } else {
+    const head = {
+      'Content-Length': fileSize,
+      'Content-Type': 'video/mp4',
+    }
+    res.writeHead(200, head)
+    fs.createReadStream(path).pipe(res)
+  }
+});
+
 var port = process.env.HOST_PORT || process.env.VCAP_APP_PORT || 3000;
 http.listen(port, function() {
     console.log('Server running on port: %d', port);
